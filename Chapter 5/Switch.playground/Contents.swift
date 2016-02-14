51af6db393227f8975074ce868e9d00d6ea5d85c
Bob Martens <bob@bobmartens.net>
2016-02-13 21:43:59 -0600

    completed Chapter 5

--- Chapter 5/Switch.playground/Contents.swift
+++ Chapter 5/Switch.playground/Contents.swift
@@ -2,19 +2,45 @@
 
 import Cocoa
 
-var statusCode: Int = 404
+var statusCode: Int = 418
 var errorString: String = "The request failed with the error:"
 switch statusCode {
 case 100, 101:
-    errorString += " Informational, 1xx."
+    errorString += " Information, \(statusCode)"
 case 204:
     errorString += " Successful but no content, 204."
 case 300...307:
-    errorString += " Redirection, 3xx."
+    errorString += " Redirection, \(statusCode)."
 case 400...417:
-    errorString += " Client error, 4xx."
+    errorString += " Client error, \(statusCode)."
 case 500...505:
-    errorString += " Server error, 5xx."
+    errorString += " Server error, \(statusCode)."
+case let unknownCode where (unknownCode >= 200 && unknownCode < 300) || unknownCode > 505:
+    errorString = "\(unknownCode) is not a known error code."
 default:
-    errorString = "Unknown. Please review the request and try again."
+    errorString = "Unexpoected error encountered."
+}
+
+let error = (code: statusCode, error: errorString)
+error.code
+error.error
+
+let firstErrorCode = 404
+let secondErrorCode = 200
+let errorCodes = (firstErrorCode, secondErrorCode)
+
+switch errorCodes {
+case (404, 404):
+    print("No items found.")
+case (404, _):
+    print("First item not found.")
+case (_, 404):
+    print("Second item not found.")
+default:
+    print("All items found.")
+}
+
+let age = 25
+if case 18...35 = age where age >= 21 {
+    print("In cool demographic and of drinking age")
 }
