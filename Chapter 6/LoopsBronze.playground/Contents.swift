7a170a8531a7a415ac35bac39f0167f8f45cbedb
Bob Martens <bob@bobmartens.net>
2016-02-13 22:59:56 -0600

    completed Chapter 6

---
+++ Chapter 6/LoopsBronze.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var run = 1
+while run < 6 {
+    for var i = 0; i <= 100; i = i + 2 {
+        print(i)
+    }
+    ++run
+}
